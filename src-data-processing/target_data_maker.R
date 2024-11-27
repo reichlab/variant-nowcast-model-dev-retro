f234606c5cd4653dab9c65c052cd78582f5fbcda
IsaacMacarthur <161043623+IsaacMacarthur@users.noreply.github.com>
2024-11-27 11:55:35 -0500

    Making the style changes that Evan suggested

--- src-data-processing/target_data_maker.R
+++ src-data-processing/target_data_maker.R
@@ -10,14 +10,14 @@ full_data_file <- read_csv(file_path)
 
 # reducing each clade name to there 3 character designation except recombinant
 full_data_file <- full_data_file %>%
-mutate(clade = ifelse(clade == "recombinant", "recombinant", substr(clade, 1, 3)))
+   mutate(clade = ifelse(clade == "recombinant", "recombinant", substr(clade, 1, 3)))
 # creating a list of target dates to create versions of, starting for the first Monday in Aug 2022
 # and ending on the first Monday in Aug 2024
 target_days <- seq(from = as.Date("2022-08-01"), to = as.Date("2024-08-05"), by = 7)
 # for each date subsetting by date_submitted, grouping and creating a parquet file.
 # matching the structure of write_dataset
-file_name <- "/part-0.parquet"
-for(date in target_days){
+file_name <- "part-0.parquet"
+for(date in target_days[1]) {
   date <- as.Date(date)
   data_as_of_date <- full_data_file[full_data_file$date_submitted <= date, ]
   data_to_save <- data_as_of_date %>%
@@ -28,5 +28,5 @@ for(date in target_days){
   if(!file.exists(folder_name)) {
     dir.create(folder_name)
   }
-  write_parquet(data_to_save, paste0(folder_name,file_name))
+  write_parquet(data_to_save, file.path(folder_name, file_name))
 }
